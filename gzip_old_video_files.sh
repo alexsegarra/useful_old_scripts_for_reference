9530b26cd82f6477d70389ded51b81bbed11b7a7
alexsegarra <36676479+alexsegarra@users.noreply.github.com>
2020-04-24 16:56:44 -0700

    Create gzip_old_video_files.sh
    Put this in a cron-job to gzip old skype meeting videos.

---
+++ gzip_old_video_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+var=$(find ./*.mp4 -atime +7 | wc -l)
+
+if
+        [ var -ne 0 ];
+then
+        find ./*.mp4 -atime +7 -exec gzip {} \;
+        printf 'Adding the following files at"; date > gzippin_log.log
+        find ./*.mp4 -atime +7 > gzippin_log.log
+fi
